18597e55fe3293faad50ae8c25944ebef32bfb9f
Iliyan Peychev <iliyan.peychev@liferay.com>
2019-02-05 13:28:17 +0100

    Move Saucelabs token fetch service to liferay.io
    Fixes #283

--- gulpfile.js
+++ gulpfile.js
@@ -363,7 +363,7 @@ gulp.task('test:node:watch', ['test:node'], function() {
 });
 
 gulp.task('test:saucelabs', function(done) {
-  let url = 'https://app-wedeploysdkjsjwt.wedeploy.io';
+  let url = 'https://app-wedeploysdkjsjwt.lfr.io';
 
   if (process.env.TRAVIS_PULL_REQUEST) {
     url += '?pull_request=' + process.env.TRAVIS_PULL_REQUEST;
